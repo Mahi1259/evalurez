120775a8264d0465db7ce9c36333831ff273c3c7
Mahi1259 <mahidhar1337@gmail.com>
2025-03-08 13:49:51 +0100

    update api key in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type React from "react"
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
-import Footer from "../components/Footer"
-import StyledComponentsRegistry from "../lib/AntRegistry"
-import "./globals.css"
-import Navbar from "../components/Navbar"
-import { ThemeProvider } from "../components/theme-provider"
-import {dark, neobrutalism, shadesOfPurple} from "@clerk/themes"
+import type React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import Footer from "../components/Footer";
+import StyledComponentsRegistry from "../lib/AntRegistry";
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import { ThemeProvider } from "../components/theme-provider";
+import { dark, neobrutalism, shadesOfPurple } from "@clerk/themes";
 
-const inter = Inter({ subsets: ["latin"] })
+// Load Inter font
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Evalurez",
   description: "Get started today and Upload your resume for analysis!",
-}
+};
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <ClerkProvider afterSignOutUrl="/">
+  <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+  >
     <html lang="en">
       <body
         className={`${inter.className} overflow-y-scroll overflow-x-hidden`}
@@ -41,5 +44,4 @@ const RootLayout = ({ children }: React.PropsWithChildren) => (
   </ClerkProvider>
 );
 
-export default RootLayout
-
+export default RootLayout;
