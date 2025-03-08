c2f25311af63407d0b7dd837e60d3d062daef4d7
Mahi1259 <mahidhar1337@gmail.com>
2025-03-08 15:10:10 +0100

    updated root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client"; // This makes this file a client-side component
+
 import type React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
@@ -7,43 +9,48 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import { ThemeProvider } from "../components/theme-provider";
 import { dark, neobrutalism, shadesOfPurple } from "@clerk/themes";
+import { useEffect, useState } from "react";
 
 // Load Inter font
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Evalurez",
-  description: "Get started today and Upload your resume for analysis!",
-};
+const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-// const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const RootLayout = ({ children }: React.PropsWithChildren) => {
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
+  if (!isClient) {
+    return null;
+  }
 
-const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-    <html lang="en">
-      <body
-        className={`${inter.className} overflow-y-scroll overflow-x-hidden`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body
+          className={`${inter.className} overflow-y-scroll overflow-x-hidden`}
         >
-          <StyledComponentsRegistry>
-            <div className="mx-auto text-2xl gap-2 mb-10">
-              {/* Remove the header with SignedIn/SignedOut since we're moving this to Navbar */}
-              <Navbar />
-              {children}
-              <Footer />
-            </div>
-          </StyledComponentsRegistry>
-        </ThemeProvider>
-      </body>
-    </html>
-  </ClerkProvider>
-);
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <StyledComponentsRegistry>
+              <div className="mx-auto text-2xl gap-2 mb-10">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </StyledComponentsRegistry>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+};
 
 export default RootLayout;
